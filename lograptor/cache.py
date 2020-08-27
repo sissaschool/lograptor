a7ced4dc9673fdff497cbc94a1746c0c0049d8a7
Davide Brunato <brunato@sissa.it>
2020-08-27 15:44:55 +0200

    Add test module for LookupCache

--- lograptor/cache.py
+++ lograptor/cache.py
@@ -40,7 +40,7 @@ class LookupCache(object):
         self.base_gid_pattern = re.compile('^([a-zA-Z_]+)')
         ipv4_pattern = config.get('patterns', 'IPV4_ADDRESS')
         ipv6_pattern = config.get('patterns', 'IPV6_ADDRESS')
-        self.ip_pattern = re.compile(u'({0}|{1})'.format(ipv4_pattern, ipv6_pattern))
+        self.ip_pattern = re.compile('({0}|{1})'.format(ipv4_pattern, ipv6_pattern))
         self.clear()
 
     def clear(self):
@@ -69,7 +69,7 @@ class LookupCache(object):
                     return self._maps[base_gid][value]
                 else:
                     k = (len(self._maps[base_gid]) + 1) % self.mapmax
-                    new_item = u'{0}_{1:0{2}d}'.format(base_gid.upper(), k, self.mapexp)
+                    new_item = '{0}_{1:0{2}d}'.format(base_gid.upper(), k, self.mapexp)
                     self._maps[base_gid][value] = new_item
                     return new_item
             except KeyError:
@@ -81,7 +81,7 @@ class LookupCache(object):
             host = self.gethost(ip_match.group(1))
             if host == ip_match.group(1) or value.startswith(host):
                 return value
-            return u''.join([
+            return ''.join([
                 value[:ip_match.start(1)],
                 self.gethost(ip_match.group(1)),
                 value[ip_match.end(1):]])
@@ -124,7 +124,7 @@ class LookupCache(object):
                 parts.append(values[gid])
                 k = match.end(gid)
         parts.append(s[k:])
-        return "".join(parts)
+        return ''.join(parts)
 
     def gethost(self, ip_addr):
         """
